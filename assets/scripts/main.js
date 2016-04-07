07e51e8c56ecd09303f75702de25f3d1c5c0f3c6
Manuel Fosela Aguila <manuel.fonsela.contractor@bbva.com>
2016-04-08 01:05:33 +0200

    finished

---
+++ assets/scripts/main.js
@@ -0,0 +1,70 @@
+window.addEventListener('DOMContentLoaded', function(e){
+  var sshow = document.querySelector("#main-slideshow");
+  var direction = document.querySelector("#direction");
+  var controlArrows = document.querySelector("#control-arrows");
+  var controlNav = document.querySelector("#control-nav");
+  var urlDatos = document.querySelector("#data-url");
+  var slideElementSize = document.querySelector("#slide-element-size");
+  var autoplay = document.querySelector("#autoplay");
+
+  direction.addEventListener("iron-select", function(e){
+    console.log( e.target.selectedItem.innerText.replace(/^\s*/,"").replace(/\s*$/,"") );
+    sshow.direction = e.target.selectedItem.innerText.replace(/^\s*/,"").replace(/\s*$/,"");
+  });
+  direction.addEventListener("iron-activate", function(e){
+    sshow.run();
+    hideham();
+  });
+  controlArrows.addEventListener("iron-select", function(e){
+    console.log( e.target.selectedItem.innerText.replace(/^\s*/,"").replace(/\s*$/,"") );
+    sshow.controlArrows = e.target.selectedItem.innerText.replace(/^\s*/,"").replace(/\s*$/,"");
+  });
+  controlArrows.addEventListener("iron-activate", function(e){
+    sshow.run();
+    hideham();
+  });
+  controlNav.addEventListener("iron-select", function(e){
+    console.log( e.target.selectedItem.innerText.replace(/^\s*/,"").replace(/\s*$/,"") );
+    sshow.controlNav = e.target.selectedItem.innerText.replace(/^\s*/,"").replace(/\s*$/,"");
+  });
+  controlNav.addEventListener("iron-activate", function(e){
+    sshow.run();
+    hideham();
+  });
+  urlDatos.addEventListener("change", function(e){
+    console.log( e.target.value );
+    sshow.dataUrl = e.target.value;
+    sshow.run();
+    hideham();
+  });
+  slideElementSize.addEventListener("change", function(e){
+    console.log( e.target.value );
+    sshow.slideElementSize = e.target.value;
+    sshow.run();
+    hideham();
+  });
+  autoplay.addEventListener("change", function(e){
+    console.log( e.target.checked );
+    sshow.autoplay = (e.target.checked)?"1000":"0";
+    sshow.run();
+    hideham();
+  });
+
+  var ham = document.querySelector("#miniheader");
+  var hamIsActive = false;
+  ham.addEventListener("click", function(e){
+    var header = document.querySelector("#sideleft");
+    header.style.transition = "1s";
+    header.style.left = "0";
+    hamIsActive = true;
+  });
+
+  var hideham = function(){
+    if ( hamIsActive ) {
+      var header = document.querySelector("#sideleft");
+      header.style.transition = "1s";
+      header.style.left = "-400px";
+      hamIsActive = false;
+    }
+  };
+});
